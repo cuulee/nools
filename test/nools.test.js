52655a605dfcc5ce606129014216fe5d6da041bc
Doug Martin <doug@dougamartin.com>
2017-03-30 00:34:06 -0500

    Updated nools.test.js to es6

--- test/nools.test.js
+++ test/nools.test.js
@@ -1,67 +1,62 @@
-"use strict";
-var it = require("it"),
-    assert = require("assert"),
-    nools = require("../");
+'use strict';
 
-it.describe("nools", function (it) {
-    it.describe(".flow", function (it) {
-        it.should("create a flow", function () {
-            var flow = nools.flow("nools flow");
-            assert.isNotNull(flow);
-            assert.instanceOf(flow, nools.Flow);
-            assert.equal("nools flow", flow.name);
-            assert.equal(nools.getFlow("nools flow"), flow);
+const assert = require('assert');
+const nools = require('../');
+
+describe('nools', () => {
+    describe('.flow', () => {
+        it('should create a flow', () => {
+            const flow = nools.flow('nools flow');
+            assert(flow);
+            assert(flow instanceof nools.Flow);
+            assert.equal('nools flow', flow.name);
+            assert.equal(nools.getFlow('nools flow'), flow);
         });
     });
 
-    it.describe(".deleteFlow", function (it) {
-        it.should("delete a flow by name", function () {
-            var flow = nools.flow("delete nools flow");
-            assert.isNotNull(flow);
-            assert.instanceOf(flow, nools.Flow);
-            assert.equal("delete nools flow", flow.name);
-            assert.equal(nools.getFlow("delete nools flow"), flow);
+    describe('.deleteFlow', () => {
+        it('should delete a flow by name', () => {
+            const flow = nools.flow('delete nools flow');
+            assert(flow);
+            assert(flow instanceof nools.Flow);
+            assert.equal('delete nools flow', flow.name);
+            assert.equal(nools.getFlow('delete nools flow'), flow);
 
-            assert.equal(nools.deleteFlow("delete nools flow"), nools);
-            assert.isUndefined(nools.getFlow("delete nools flow"));
-
+            assert.equal(nools.deleteFlow('delete nools flow'), nools);
+            assert(!nools.getFlow('delete nools flow'));
         });
 
-        it.should("delete a flow using a Flow instance", function () {
-            var flow = nools.flow("delete nools flow");
-            assert.isNotNull(flow);
-            assert.instanceOf(flow, nools.Flow);
-            assert.equal("delete nools flow", flow.name);
-            assert.equal(nools.getFlow("delete nools flow"), flow);
+        it('should delete a flow using a Flow instance', () => {
+            const flow = nools.flow('delete nools flow');
+            assert(flow);
+            assert(flow instanceof nools.Flow);
+            assert.equal('delete nools flow', flow.name);
+            assert.equal(nools.getFlow('delete nools flow'), flow);
 
             assert.equal(nools.deleteFlow(flow), nools);
-            assert.isUndefined(nools.getFlow("delete nools flow"));
-
+            assert(!nools.getFlow('delete nools flow'));
         });
     });
 
-    it.describe(".hasFlow", function (it) {
-
-        it.should("return true if the flow exists", function () {
-            var name = "has flow";
+    describe('.hasFlow', () => {
+        it('should return true if the flow exists', () => {
+            const name = 'has flow';
             nools.flow(name);
-            assert.isTrue(nools.hasFlow(name));
+            assert(nools.hasFlow(name));
         });
 
-        it.should("return false if the flow does not exists", function () {
-            assert.isFalse(nools.hasFlow(new Date().toString()));
+        it('should return false if the flow does not exists', () => {
+            assert(!nools.hasFlow(new Date().toString()));
         });
     });
 
-    it.describe(".deleteFlows", function (it) {
-
-        it.should("deleteAllFlows", function () {
-            var name = "delete nools flows";
+    describe('.deleteFlows', () => {
+        it('should deleteAllFlows', () => {
+            const name = 'delete nools flows';
             nools.flow(name);
-            assert.isTrue(nools.hasFlow(name));
+            assert(nools.hasFlow(name));
             assert.equal(nools.deleteFlows(), nools);
-            assert.isFalse(nools.hasFlow(name));
+            assert(!nools.hasFlow(name));
         });
-
     });
 });
